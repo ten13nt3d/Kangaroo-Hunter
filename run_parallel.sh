3518bbcbb8f138ac359ef1c3c02bebd8260b1d98
ten13nt3d <ten13nt3d@users.noreply.github.com>
2025-04-20 01:42:48 +0200

    (feat) x rights & puzzle69 hash160

--- run_parallel.sh
+++ run_parallel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TARGET="PUT_YOUR_TARGET_HASH_HERE"
+TARGET="61eb8a50c86b0584bb727dd65bed8d2400d6d5aa"
 PARALLEL_JOBS=1  # Para 1 GPU, puedes dejar 1. Si tienes más, aumenta.
 
 docker-compose build
